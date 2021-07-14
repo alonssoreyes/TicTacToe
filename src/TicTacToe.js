4005c16f2407fcbfbb7cd5d999968554f9cc7dc2
Alonso Reyes <alonso.reyes@ibm.com>
2021-07-14 13:37:44 -0500

    Scenario 1 GREEN

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,6 +1,41 @@
 const TicTacToe = { 
     printBoard: function(){
-        return '';
+         this.board = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""],
+          ]
+
+          let printedBoard = "";
+
+          // First row
+          printedBoard += this.board[0][0] == "" ? "  " : ` ${this.this.board[0][0]}`;
+          printedBoard += "|";
+          printedBoard += this.board[0][1] == "" ? " " : `${this.board[0][1]}`;
+          printedBoard += "|";
+          printedBoard += this.board[0][2] == "" ? "  \n" : `${this.board[0][2]}\n`;
+      
+          // Filler
+          printedBoard += "--|-|--\n";
+      
+          // Second row
+          printedBoard += this.board[1][0] == "" ? "  " : ` ${this.board[1][0]}`;
+          printedBoard += "|";
+          printedBoard += this.board[1][1] == "" ? " " : `${this.board[1][1]}`;
+          printedBoard += "|";
+          printedBoard += this.board[1][2] == "" ? "  \n" : `${this.board[1][2]}\n`;
+      
+          // Filler
+          printedBoard += "--|-|--\n";
+      
+          // Third row
+          printedBoard += this.board[2][0] == "" ? "  " : ` ${this.board[2][0]}`;
+          printedBoard += "|";
+          printedBoard += this.board[2][1] == "" ? " " : `${this.board[2][1]}`;
+          printedBoard += "|";
+          printedBoard += this.board[2][2] == "" ? "  \n" : `${this.board[2][2]}\n`;
+          console.log(printedBoard)
+          return printedBoard;
     }
 }
 
--- test/TicTacToe.test.js
+++ test/TicTacToe.test.js
@@ -2,9 +2,5 @@ const { expect } = require("@jest/globals");
 const TicTacToe = require("../src/TicTacToe");
 
 test('Printed Board', () => {
-    expect(TicTacToe.printBoard()).toBe(`  | |
-        --|-|--
-          | |
-        --|-|--
-          | |`);
+    expect(TicTacToe.printBoard()).toBe("  | |  \n--|-|--\n  | |  \n--|-|--\n  | |  \n");
 })
