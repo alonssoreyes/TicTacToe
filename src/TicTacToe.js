7a7563f8af71b99d11e2155f7a1ca583895c1786
Alonso Reyes <alonso.reyes@ibm.com>
2021-07-14 17:37:57 -0500

    Scenario 3 RED

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -56,9 +56,17 @@ const TicTacToe = {
           return printedBoard;
     },
     checkWin: function (symbol,board){
-        return [board[0][0] === symbol &&
-        board[1][0] === symbol &&
-        board[2][0] === symbol].includes(true)
+        return [// Columns
+                board[0][0] === symbol &&
+                board[1][0] === symbol &&
+                board[2][0] === symbol,
+                board[0][1] === symbol &&
+                board[1][1] === symbol &&
+                board[2][1] === symbol,
+                board[0][2] === symbol &&
+                board[1][2] === symbol &&
+                board[2][2] === symbol,
+            ].includes(true)
 
     }
 }
--- test/TicTacToe.test.js
+++ test/TicTacToe.test.js
@@ -28,4 +28,56 @@ describe('Scenario 2', () => {
 
     })
 })
+describe('Scenario 2', () => {
+    test('User X Wins in a vertial line', () => {
+        const symbol = "X";
+        let scenarioXWinFirstColumn = [
+            [symbol, "", ""],
+            [symbol, "", ""],
+            [symbol, "", ""],
+        ]
+        let scenarioXWinSecondColumn = [
+            ["", symbol, ""],
+            ["", symbol, ""],
+            ["", symbol, ""],
+        ]
+        let scenarioXWinThirdColumn = [
+            ["", "", symbol],
+            ["", "", symbol],
+            ["", "", symbol],
+        ]
+    
+        expect(TicTacToe.checkWin(symbol,scenarioXWinFirstColumn)).toStrictEqual(true);
+        expect(TicTacToe.checkWin(symbol,scenarioXWinSecondColumn)).toStrictEqual(true);
+
+        expect(TicTacToe.checkWin(symbol,scenarioXWinThirdColumn)).toStrictEqual(true);
+
+        expect(typeof TicTacToe.checkWin(symbol,scenarioXWinFirstColumn)).toBe('boolean');
+        expect(TicTacToe.checkWin("O",scenarioXWinFirstColumn)).toBe(false);
+
+    })
+})
+describe('Scenario 3', () => {
+    test('User "O"  Wins in a horizontal line', () => {
+        const symbol = "O";
+        let scenarioOWinFirstRow = [
+            [symbol, symbol, symbol],
+            ["", "", ""],
+            ["", "", ""],
+        ]
+        let scenarioOWinSecondRow = [
+            ["", "", ""],
+            [symbol, symbol, symbol],
+            ["", "", ""],
+        ]
+        let scenarioOWinThirdRow = [
+            ["", "", ""],
+            ["", "", ""],
+            [symbol, symbol, symbol],
+        ]
+
+        expect(TicTacToe.checkWin(symbol,scenarioOWinFirstRow)).toBe(true)
+    })
+    
+})
 
