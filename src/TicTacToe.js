582d2222c4234b7686d66d04049d07239a1580c7
Alonso Reyes <alonso.reyes@ibm.com>
2021-07-14 23:00:58 -0500

    FINAL REFACTOR , this is an extra only for complete the game

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,11 +1,11 @@
 const TicTacToe = {
     
     setup:function(){
-        this.PLAYER1 = {
+        this.player1 = {
             name:"Cross",
             symbol:"X"
         };
-        this.PLAYER2 = {
+        this.player2 = {
             name:"Naughts",
             symbol:"O"
         };
@@ -18,8 +18,8 @@ const TicTacToe = {
             ["", "", ""],
             ["", "", ""],
         ]
-        this.currentPlayer = Math.floor(Math.random() * 2)== 0 ? this.PLAYER1 : this.PLAYER2;
-        this.printBoard(this.board)
+        this.currentPlayer = Math.floor(Math.random() * 2)== 0 ? this.player1 : this.player2;
+        this.play();
     } ,
 
     printBoard: function(board){
@@ -53,6 +53,9 @@ const TicTacToe = {
           printedBoard += board[2][1] == "" ? " " : `${board[2][1]}`;
           printedBoard += "|";
           printedBoard += board[2][2] == "" ? "  \n" : `${board[2][2]}\n`;
+          console.log(printedBoard)
+          console.log("")
+
           return printedBoard;
     },
     checkWin: function (symbol,board){
@@ -87,12 +90,48 @@ const TicTacToe = {
         ].includes(true)
 
     },
+    playerTurn: function (player) {
+        
+        while (true) {
+          let currentPosition = [Math.floor(Math.random() * 3), Math.floor(Math.random() * 3)];
+          if (this.board[currentPosition[0]][currentPosition[1]] == "") {
+            this.board[currentPosition[0]][currentPosition[1]] = player.symbol;
+            this.printBoard(this.board);
+            if (this.checkWin(player.symbol, this.board)) {
+              console.log(this.currentPlayer.name + " win!");
+              this.playAgainDialogue();
+            } else if (this.isDraw(this.board)) {
+              console.log("It's a draw");
+              this.playAgainDialogue();
+            }
+            this.currentPlayer =
+              this.currentPlayer == this.player1 ? this.player2 : this.player1;
+            break;
+          }
+          continue;
+        }
+      },
+    play: function() {
+
+        while(!this.checkWin(this.player1.symbol, this.board) && !this.checkWin(this.player2.symbol, this.board) && !this.isDraw(this.board)){
+            this.sleep(500);
+            this.playerTurn(this.currentPlayer);
+        }
+    },
     isDraw: function(board) { 
         return (
             !this.checkWin("X", board) &&
-            !this.checkWin("O", board)
+            !this.checkWin("O", board) && 
+            !this.board.some((row) => row.includes(""))
           );
-    }
+    },
+    sleep : function (delay) {
+        const start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
+    },
+    playAgainDialogue: function () {
+        console.log("Type node src/TicTacToe.js to play again");
+      },
 }
-
+TicTacToe.setup();
 module.exports = TicTacToe;
